e5dcb65c85d1fe750f667f26538e3fdbb888c9cd
manojpandey991 <mktechm@gmail.com>
2021-09-30 12:29:11 +0530

    added saveItem method in HelloController

--- src/main/java/com/mk/unittesting/controller/HelloController.java
+++ src/main/java/com/mk/unittesting/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.mk.unittesting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,11 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String hello() {
 		return"Hello world";
+	}
+	
+	@PostMapping("/items")
+	public Item saveItem() {
+		service.saveItem();
 	}
 
 }
